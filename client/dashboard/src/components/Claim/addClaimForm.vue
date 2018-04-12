93f295ab408d11bbc0790559fa57dbe8e055a392
Amir <amir.mogaadi@esprit.tn>
2018-04-12 20:55:06 +0100

    add maps

--- client/dashboard/src/components/Claim/addClaimForm.vue
+++ client/dashboard/src/components/Claim/addClaimForm.vue
@@ -1,4 +1,5 @@
 <template>
+
   <card>
     <h4 slot="header" class="card-title">Add Claim</h4>
      <b-form @submit="onSubmit">
@@ -44,14 +45,37 @@
           Add Claim
         </button>
       </div>
+       <gmap-map
+         :center="center"
+         :zoom="7"
+         style="width: 500px; height: 300px"
+       >
+         <gmap-marker
+           :key="index"
+           v-for="(m, index) in markers"
+           :position="m.position"
+           :clickable="true"
+           :draggable="true"
+           @click="center=m.position"
+         ></gmap-marker>
+       </gmap-map>
       <div class="clearfix"></div>
     </b-form>
   </card>
 </template>
 <script>
   import Card from 'src/components/UIComponents/Cards/Card.vue'
-
+  import * as VueGoogleMaps from 'vue2-google-maps';
+  import Vue from 'vue';
   import axios from 'axios'
+
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyDKzdqvMBg_X79-RNMpBUeRe-b4AGiRpH8',
+      v: 'OPTIONAL VERSION NUMBER',
+      // libraries: 'places', //// If you need to use place input
+    }
+  });
 
   export default {
     name: 'CreteObject',
@@ -64,7 +88,13 @@
         type: ['', 'info', 'success', 'warning', 'danger'],
         notifications: {
           topCenter: false
-        }
+        },
+        center: {lat: 10.0, lng: 10.0},
+        markers: [{
+          position: {lat: 10.0, lng: 10.0}
+        }, {
+          position: {lat: 11.0, lng: 11.0}
+        }]
       }
     },
     methods: {
